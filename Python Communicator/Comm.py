5ffdcfed3c91f98a682c9b051c17be29f1f848e3
Cairin Michie <seamouse3@gmail.com>
2016-04-21 06:32:14 +0200

    Added python comm module

---
+++ Python Communicator/Comm.py
@@ -0,0 +1,39 @@
+import time
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+#ser = serial.Serial(
+#    port='/dev/ttyUSB1',
+#    baudrate=10000
+#)
+
+#ser.isOpen()
+
+print('Welcome to The Fourier Awakens CLI')
+print('Enter your commands below.\nInsert "exit" to leave the application.')
+print('The following waveforms are available:\n\tWaveform 1\n\tWaveform 2\n\tWaveform 3\n\tWaveform 4')
+print('Enter your commands below.\nInsert "exit" to leave the application.')
+
+
+#loop for sending serial data - may need to make a function.
+while 1:
+    # get keyboard input
+    command = input(">> ")
+    if command == 'exit':
+        #ser.close()
+        break
+    else:
+        # send the character to the device
+        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+        #ser.write(input)
+        print(command)
+
+        #For receiving stuff.
+        #out = ''
+        # let's wait one second before reading output (let's give device time to answer)
+        #time.sleep(1)
+        #while ser.inWaiting() > 0:
+            #out += ser.read(1)
+
+        #if out != '':
+            #print(">>" + out)
---
+++ Python Communicator/Readme.md
@@ -0,0 +1,7 @@
+# The Fourier Awakens
+## Python communicator module
+
+####To do:
+
+- [ ] Get serial communication running
+- [ ] Decide on protocol
