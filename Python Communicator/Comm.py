1f38a3737d30ca6d77ab936a0af8ba5119b7238b
Cairin Michie <cairinmichie@gmail.com>
2016-04-21 16:23:21 +0200

    Update Comm.py

--- Python Communicator/Comm.py
+++ Python Communicator/Comm.py
@@ -1,64 +1,78 @@
 
 import time
-#import serial
+import serial
+import os
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
-#ser = serial.Serial(
-#    port='/dev/ttyUSB1',
-#   baudrate=10000
-#)
+ser = serial.Serial(
+    port='/dev/ttyUSB1',
+   baudrate=10000
+)
 
-#ser.isOpen()
+def isInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+ser.isOpen()
 
 print('Welcome to The Fourier Awakens CLI')
-print('Use the following commands to control the output:\n\ta - Choose the amplitude.\n\tc - Choose the number of Fourier Coefficients.')
-print('\tf - Choose the frequency.\n\tw - Choose the synthesised waveform.\n\te - Exit application.')
+
 
 
 #loop for sending serial data - may need to make a function.
 while 1:
     # get keyboard input
-    command = input(">> ")
+    print('Use the following commands to control the output:\n\ta - Choose the amplitude.\n\tc - Choose the number of Fourier Coefficients.')
+    print('\tf - Choose the frequency.\n\tw - Choose the synthesised waveform.\n\te - Exit application.')
+    command = raw_input(">> ")
     if(command == 'e'):
-#        ser.close()
+        ser.close()
+        os.system('cls' if os.name == 'nt' else 'clear')
         break
     elif(command == 'a'):
         print('Set an amplitude for the DDS.')
-        amp = input("\n>> ")
+        amp = raw_input("\n>> ")
         if(isInt(amp)):
             print(amp)
-#            ser.write(amp)
+            ser.write(amp)
         else:
             print("Input was not an integer.")
+        os.system('cls' if os.name == 'nt' else 'clear')
     elif(command == 'c'):
         print('Choose a number of coefficients (from 1-16):')
-        coe = input("\n>> ")
+        coe = raw_input("\n>> ")
         if(isInt(coe)):
             print(coe)
-#            ser.write(coe)
+            ser.write(coe)
         else:
             print("Input was not an integer.")
+        os.system('cls' if os.name == 'nt' else 'clear')
     elif(command == 'f'):
         print('Set a frequency for the DDS.')
-        freq = input("\n>> ")
+        freq = raw_input("\n>> ")
         if(isInt(freq)):
             print(freq)
-#            ser.write(freq)
+            ser.write(freq)
         else:
             print("Input was not an integer.")
+        os.system('cls' if os.name == 'nt' else 'clear')
     elif(command == 'w'):
         print('The following waveforms are available:\n\t1 - square\n\t2 - triangle\n\t3 - sawtooth\n\t4 - bonus')
-        wav = input("\n>> ")
+        wav = raw_input("\n>> ")
         if(isInt(wav)):
             print(wav)
-#            ser.write(wav)
+            ser.write(wav)
         else:
             print("Input was not an integer.")
+        os.system('cls' if os.name == 'nt' else 'clear')
     else:
         # send the character to the device
         #ser.write(command)
         print('Command not valid.')
-
+        os.system('cls' if os.name == 'nt' else 'clear')
         #For receiving stuff.
         #out = ''
         # let's wait one second before reading output (let's give device time to answer)
@@ -70,9 +84,4 @@ while 1:
             #print(">>" + out)
 
 
-def isInt(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False
+
